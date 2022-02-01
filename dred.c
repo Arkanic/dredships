9c9c7f706985e9a77cc46ab76f7c9dc0f9bfc636
Arkanic <silverfishasaurus@gmail.com>
2022-02-01 03:34:11 +0000

    basic loop

--- dred.c
+++ dred.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "dred.h"
 
@@ -7,17 +8,26 @@ int main(void) {
 	struct string s;
 	initstr(&s);
 
-	char url[256];
-	snprintf(url, 256, "https://drednot.io/api/scoreboard?count=%d", GRAB_AMOUNT);
+	int offset = 0;
+	while(1) {
+		char url[256];
+		char offseturl[64];
+		if(offset != 0) snprintf(offseturl, 64, "&offset_score=%d", offset);
+		snprintf(url, 256, "https://drednot.io/api/scoreboard?count=%d%s", GRAB_AMOUNT, offseturl);
+		getstr(&s, url);
 
-	getstr(&s, url);
+		struct ship buf[GRAB_AMOUNT];
+		parsetext(s.str, buf);
 
-	struct ship buf[1000];
+		int newscore = buf[GRAB_AMOUNT - 1].score;
+		char *newname = strdup(buf[GRAB_AMOUNT - 1].name);
 
-	parsetext(s.str, buf);
-	for(int i = 0; i < GRAB_AMOUNT; i++) {
-		printf("%s\n", buf[i].name);
+		printf("%s - %d\n", newname, newscore);
+		if(offset == newscore) break;
+		offset = newscore;
 	}
+
+	printf("Done.\n");
 
 	return 0;
 }
